d4f1debb57fed52efebd2e6879c8e4a45380d541
zxqlxy <liuxinyun6@yahoo.com>
2021-06-29 18:34:33 -0500

    update config

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-start = "2013-11-09 23:00"
-end = "2013-11-10 11:59"
+from datetime import timedelta
+
+delta = timedelta(hours = 1)
+
+# start = "2013-11-09 23:00"
+# end = "2013-11-10 11:59"
+start = "2011-03-01 00:00"
+end = "2011-03-31 23:00"
+report_name = "goes-xrs-report_2011.txt"
+wavelength = "094"
