0b0d84ac2ed6b845a843af9cfed08da676d81ba8
zxqlxy <liuxinyun6@yahoo.com>
2021-07-01 15:05:31 -0500

    add 404 check

--- download.py
+++ download.py
@@ -9,13 +9,11 @@ from datetime import datetime, timedelta
 from os import path
 import time as t
 
-from config import start, end, delta, wavelength
+from config import start, end, delta, wavelength, locPath
 
-base = "/Users/lxy/Desktop/Rice/Su 21/Solar_Weather_Forecast/data/SDOAIA/" + wavelength + "/"
 start_time = datetime.strptime(start, '%Y-%m-%d %H:%M')
 end_time = datetime.strptime(end, '%Y-%m-%d %H:%M')
 urlBase = "http://jsoc.stanford.edu/data/aia/synoptic"
-locPath = "/Users/lxy/Desktop/Rice/Su 21/Solar_Weather_Forecast/data/SDOAIA/" + wavelength + "/"
 count = (end_time - start_time) // delta            # number of data points
 
 def download(myUrl, myDest):
@@ -25,6 +23,8 @@ def download(myUrl, myDest):
     print(myUrl)
     myBits = requests.get(myUrl)
     t.sleep(1)
+    if myBits.status_code == 404:
+        return "file not exists"
 
     if len(myBits.content) > 500: # if files don't exist we get a short reply from the server: skip these
         myFile = open(myDest, "wb")
@@ -46,7 +46,7 @@ while time < end_time:
     da = str(time.day) if time.day >= 10 else '0' + str(time.day)
     ho = str(time.hour) if time.hour >= 10 else '0' + str(time.hour)
     mi = str(time.minute) if time.minute >= 10 else '0' + str(time.minute)
-    thisFile = "AIA"+ yr + mo + da + "_" + ho + mi + "_0171.fits"
+    thisFile = "AIA"+ yr + mo + da + "_" + ho + mi + "_0" + wavelength + ".fits"
     thisPath = "/".join([yr,mo,da,"H" + ho + "00"])
     # print(thisFile)
 
