7de06345af314b00cf07f92d2c639567aaaf3e70
zxqlxy <liuxinyun6@yahoo.com>
2021-07-21 20:11:36 -0500

    find error in model

--- complex_binary.py
+++ complex_binary.py
@@ -11,7 +11,7 @@ import os
 import torch
 import pandas as pd
 import numpy as np
-from torch import nn
+from models import NeuralNetwork
 # import matplotlib.pyplot as plt
 import argparse
 
@@ -45,62 +45,8 @@ if device == 'cuda' and args.use_benchmark:
 
 """
 
-class DenseBlock(nn.Module):
-    def __init__(self, n):
-        super().__init__()
-        inplanes = ((n)*3 + 2) * 13
-        self.bn1 = nn.BatchNorm2d(inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(inplanes, (n+1)*13, kernel_size=1, stride=1)
-        self.bn2 = nn.BatchNorm2d((n+1)*13)
-        self.conv2 = nn.Conv2d((n+1)*13, 39, kernel_size=3, stride=1,padding=1) # Must add padding
-        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-    
-    def forward(self,x):
-        identity = x
-
-        out = self.bn1(x)
-        out = self.relu(out)
-        out = self.conv1(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-
-        # out += identity
-        out = torch.cat((out, identity), dim=1)
-
-        out = self.pool(out)
-
-        return out
-
-class NeuralNetwork(nn.Module):
-    def __init__(self, num_dense):
-        super().__init__()
-        self.num_dense = num_dense
-        self.initial = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=26, kernel_size=3),
-            nn.MaxPool2d(2, stride=2))
-        self.end = nn.Sequential(
-            nn.BatchNorm2d((2+3*5)*13),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-        )
-        self.denseblocks = [DenseBlock(num).to(device) for num in range(self.num_dense)]
-        # not sure what the input dimension is
-        self.fc = nn.Linear(221, 1)
-
-    def forward(self, x):
-        out = self.initial(x)
-
-        for num in range(self.num_dense):
-            out = self.denseblocks[num](out)
-
-        out = self.end(out)
-        out = torch.flatten(out, 1)
-        out = self.fc(out)
-        return out
-
 model = NeuralNetwork(5).to(device)
-# print(model)
+print(model)
 
 """### Loss and Optimize"""
 
---
+++ models/neural_net.py
@@ -0,0 +1,63 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+from torch import nn
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, n):
+        super().__init__()
+        inplanes = ((n)*3 + 2) * 13
+        self.add_module('bn1', nn.BatchNorm2d(inplanes))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv1', nn.Conv2d(inplanes, (n+1)*13, kernel_size=1, stride=1))
+        self.add_module('bn2', nn.BatchNorm2d((n+1)*13))
+        self.add_module('conv2', nn.Conv2d((n+1)*13, 39, kernel_size=3, stride=1,padding=1)) # Must add padding
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+    
+    def forward(self,x):
+        identity = x
+
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        # out += identity
+        out = torch.cat((out, identity), dim=1)
+
+        out = self.pool(out)
+
+        return out
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, num_dense):
+        super().__init__()
+        self.num_dense = num_dense
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=26, kernel_size=3),
+            nn.MaxPool2d(2, stride=2))
+        self.end = nn.Sequential(
+            nn.BatchNorm2d((2+3*5)*13),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+        )
+        for i in range(self.num_dense):
+            block = DenseBlock(i)
+            self.initial.add_module('denseblock%d' % (i + 1), block)
+        # self.denseblocks = [.to(device) for num in range(self.num_dense)]
+        # not sure what the input dimension is
+        self.fc = nn.Linear(221, 1)
+
+    def forward(self, x):
+        out = self.initial(x)
+
+        # for num in range(self.num_dense):
+        #     out = self.denseblocks[num](out)
+
+        out = self.end(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
